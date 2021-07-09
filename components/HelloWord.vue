4dcfacd5389c068b44eeba0cdb2449b77c94b53b
张大壮 <oncwnuP3jH6PK6TKHpEeIC-rJtys@git.weixin.qq.com>
2021-07-09 11:53:45 +0800

    初始化1.0.1

---
+++ components/HelloWord.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="hello-word">HelloWord</div>
+</template>
+
+<script>
+module.exports = {
+	name: 'HelloWord',
+	data() {
+		return {};
+	},
+	methods: {},
+	created() {}
+};
+</script>
+
+<style></style>
